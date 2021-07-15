1fbf6561c807c380d8ddba12a47462a826819c17
Andraz Filipcic <afilipci@tave101-eth4.cscs.ch>
2021-07-15 13:31:33 +0200

    update

--- sort.sh
+++ sort.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-for b in 1 2 4 8 16 32
+for b in 1 2 4 8 16 32 64
 do
 	for a in 1 2 4 8 16 32
 	do
-		grep WRITE $b-node/randwrite$a.log | cut -d = -f 2 | cut -d M -f 1 > $b-node/speeds$a
-		awk '{total+=$1} END {print total}' $b-node/speeds$a >> $b-node/speeds$a
-		echo -n "$b nodes, $a jobs/node, $(($b*$a)) total jobs = " >> tmpSort
-		tail -n 1 $b-node/speeds$a >> tmpSort	
+		rm tmpSort
+		grep WRITE randwrite/$b-node/randwrite$a.log | cut -d = -f 2 | cut -d i -f 1 > randwrite/$b-node/speeds$a
+		cat randwrite/$b-node/speeds$a | while read line || [[ -n $line ]]
+		do
+			if [ "${line: -1}" == "K" ]
+			then
+				echo "${line:0:1}.${line:1}" >> tmpSort
+			else
+				echo $line >> tmpSort
+			fi
+		done
+		sed -i 's/.$//' tmpSort
+		awk '{total+=$1} END {print total}' tmpSort >> tmpSort
+		echo -n "$b nodes, $a jobs/node, $(($b*$a)) total jobs = " >> tmpSortA
+		tail -n 1 tmpSort >> tmpSortA
 	done
 done
 
-sort -nrk 9 tmpSort > sorted
+sort -nrk 9 tmpSortA > randwrite/sortedWrite
+rm tmpSortA
 rm tmpSort
