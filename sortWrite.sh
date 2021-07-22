6366c922af47b837176bb475192f38e78d5833a8
Andraz Filipcic <afilipci@tave101-eth4.cscs.ch>
2021-07-22 13:45:23 +0200

    Moved bw to bws

--- sortWrite.sh
+++ sortWrite.sh
@@ -2,7 +2,7 @@
 
 for b in 1 2 4 8 16 32 64 96 128 150
 do
-	rm randwrite/bw$b
+	rm randwrite/bws/bw$b
 	for a in 1 2 4 8 16 32 64
 	do
 		grep WRITE randwrite/$b-node/randwrite$a.log | cut -d = -f 2 | cut -d i -f 1 > randwrite/$b-node/speeds$a
@@ -19,7 +19,7 @@ do
 		awk '{total+=$1} END {print total}' tmpSort >> tmpSort
 		echo -n "$b nodes $a jobs ($(($b*$a)) total jobs); " >> tmpSortA
 		echo -n " $(tail -n 1 tmpSort) MB/s ; iops: " >> tmpSortA
-		tail -n 1 tmpSort >> randwrite/bw$b
+		tail -n 1 tmpSort >> randwrite/bws/bw$b
 
 		grep IOPS randwrite/$b-node/randwrite$a.log | cut -d = -f 2 | cut -d , -f 1 > randwrite/$b-node/iops$a
 		awk '{n += $1}; END{print n}' randwrite/$b-node/iops$a >> tmpSortA
